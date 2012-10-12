860c99c244773be412db759933bb71f753abf6dc
Yorick <github@nextpulse.nl>
2012-10-12 13:04:39 +0200

    Updated sample json and altered the placeholder content

---
+++ survey_viewer/assets/www/js/application.js
@@ -0,0 +1,37 @@
+$(function() {
+
+	// Temporarily include the dataset in this file to prevent CORS issues when opening index.html in a local browser
+	var input_data = {
+	   "question block":{
+	      "question":{
+	         "id":"xxxx",
+	         "label":"xxxxxxxx",
+	         "type":{
+	            "text":"blabla",
+	            "multi select":{
+	               "options":[
+	                  {
+	                     "value":"mozart"
+	                  },
+	                  {
+	                     "value":"bach"
+	                  }
+	               ]
+	            },
+	            "single select":{
+	               "options":[
+	                  {
+	                     "value":"mozart"
+	                  },
+	                  {
+	                     "value":"bach"
+	                  }
+	               ]
+	            }
+	         }
+	      }
+	   }
+	};
+	
+	console.log(input_data);
+});
