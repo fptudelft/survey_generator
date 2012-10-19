55b6f92ec205003c0697b6bfbe07255b9b9a2a24
Yorick Holkamp <github@nextpulse.nl>
2012-10-19 13:28:55 +0200

    Updated database format and the server is now handling answer submissions properly

--- survey_server/server.js
+++ survey_server/server.js
@@ -34,34 +34,13 @@ exports.start = function () {
      * @param response
      */
     function showSurveyIndex(response) {
-        writeHeader('application/json', response).end(JSON.stringify(surveyList()));
-    }
-
-    /**
-     * Produces a list of surveys ready for presentation
-     * @return {Array}
-     */
-    function surveyList() {
-        // TODO: Retrieve survey list from database
-        return [
-            {
-                "id":"1234",
-                "title":"Foo"
-            },
-            {
-                "id":"42",
-                "title":"Bar"
+        client.query('SELECT id, title FROM surveys', function (error, rows, fields) {
+            if (error) {
+                throw error;
             }
-        ];
-    }
 
-    /**
-     * Retrieve a survey from storage
-     * @param id
-     * @return {Survey}
-     */
-    function getSurvey(id) {
-        return require("./sample_survey.js").survey;
+            response.end(JSON.stringify(rows));
+        });
     }
 
     /**
@@ -69,8 +48,8 @@ exports.start = function () {
      * @param url
      * @param response
      */
-    function showSurvey(url, response) {
-        writeHeader('application/json', response).end(JSON.stringify(getSurvey(url)));
+    function showSurvey(id, response) {
+        // TODO, implement correctly in a neat way
     }
 
     /**
@@ -79,7 +58,67 @@ exports.start = function () {
      * @param response
      */
     function processAnswers(request, response) {
-        // TODO: implement
+        var post_data = '';
+        request.on('data', function (input) {
+            post_data += input.toString();
+        });
+        request.on('end', function () {
+            response.end(JSON.stringify(storeAnswers(getAnswersData(post_data))));
+        });
+    }
+
+    /**
+     * Parses a post string containing JSON and returns a Javascript object
+     * @param post_string
+     * @return {*}
+     */
+    function getAnswersData(post_string) {
+        return JSON.parse(require('querystring').parse(post_string).data);
+    }
+
+    /**
+     * Performs validation using the JSON Schema on the provided answers object
+     * @param answers
+     * @return {*}
+     */
+    function validateAnswers(answers) {
+        return require("./lib/json-schema").validate(answers, require("./survey_schemas.js").survey_entry_schema);
+    }
+
+    /**
+     * Attempts to validate and store answers in the database
+     * @param answer_object
+     * @return {Object}
+     */
+    function storeAnswers(answer_object) {
+        if (validateAnswers(answer_object).errors.length != 0) {
+            return {success:false, errors:validateAnswers(answer_object).errors};
+        } else {
+            return {
+                success:insertAnswers(answer_object).reduce(function (previous, current) {
+                    return previous && current;
+                }, true)
+            }
+        }
+    }
+
+    /**
+     * Performs the database insertion
+     * @param answer_object
+     * @return {*}
+     */
+    function insertAnswers(answer_object) {
+        return extractAnswerValues(answer_object).map(function (answer_value) {
+            // TODO: Perform error handling
+            client.query("INSERT INTO answers (question_id, group_id, answer) VALUES (?,?,?)", answer_value);
+            return true;
+        });
+    }
+
+    function extractAnswerValues(answer_object) {
+        return answer_object['answers'].map(function (answer) {
+            return [answer.id.split('_')[0], answer.id.split('_')[1], answer.answer];
+        });
     }
 
     /**
@@ -89,39 +128,46 @@ exports.start = function () {
      */
     function onRequest(request, response) {
         switch (routingPath(request.url)) {
+            case "favicon.ico":
+                break;
             case "surveys":
                 showSurveyIndex(response);
                 break;
             case "survey":
-                showSurvey(request.url, response);
+                showSurvey(request.url.split('/')[2], response);
                 break;
             case "answers":
                 processAnswers(request, response);
                 break;
-            case "/favicon.ico":
+            case "test":
+                showTest(response);
                 break;
             default:
                 produceIndex(response);
                 break;
         }
 
-        test_schema_validation();
+//        test_schema_validation();
     }
 
-    function test_schema_validation() {
-        object = {
-            "id": "1234",
-            "answers": [
-                {
-                    "id": "group_questionid",
-                    "answer": "foo"
-                }
-            ]
-        };
-        console.log(require("./lib/json-schema").validate(object, require("./survey_schemas.js").survey_entry_schema).errors.length);
+    function showTest(response) {
+        require('fs').readFile('./test.html', function (error, content) {
+            if (error) {
+                response.writeHead(500);
+                response.end();
+            }
+            else {
+                response.writeHead(200, { 'Content-Type':'text/html' });
+                response.end(content, 'utf-8');
+            }
+        });
     }
 
     // Start the server and print some helpful information to console
     require('http').createServer(onRequest).listen(3000);
     console.log('Server running at http://127.0.0.1:3000/');
+
+    // Connect to MySQL
+    var client = require('mysql').createClient({user:'survey_server', password:"foobar"});
+    client.query('USE survey_server');
 };
