2e58ccf570be97153616baf31fb0c700d0530ecc
Yorick Holkamp <github@nextpulse.nl>
2012-11-01 21:18:15 +0100

    Added JSONP support to the server for proper cross-origin calls and restructured some of the boiler plate code.

--- survey_server/server.js
+++ survey_server/server.js
@@ -6,46 +6,109 @@ exports.start = function () {
      */
     function onRequest(request, response) {
         console.log(request.url);
+        findControllerAction(routingPath(request.url))(request, response);
+    }
 
-        switch (routingPath(request.url)) {
-            case "surveys":
-                showSurveyIndex(response);
-                break;
-            case "survey":
-                showSingleSurvey(request.url.split('/')[2], response);
-                break;
-            case "completed_survey":
-                processAnswers(request, response);
-                break;
-            case "test":
-                showTestPage(response);
-                break;
-            default:
-                response.end("\n");
+    /**
+     * Returns an hash of functions handling requests for the listed url segments.
+     * @return {Object}
+     */
+    function routes() {
+        return {
+            "surveys":showSurveyIndex,
+            "survey":showSurvey,
+            "completed_survey":processAnswers,
+            "test":showTestPage,
+            "favicon.ico":function () {
+            }
+        };
+    }
+
+    /**
+     * Returns a function that will handle the
+     * @param url_part
+     * @return {*}
+     */
+    function findControllerAction(url_part) {
+        return routes()[url_part] || function (request, response) {
+            console.log('No controller action defined for url segment ' + url_part);
+            response.end("\n");
+        };
+    }
+
+    /**
+     * Returns the value of the 'callback' GET parameter
+     * @param request
+     * @return string | undefined
+     */
+    function retrieveCallbackFromRequest(request) {
+        return require('url').parse(request.url, true)['query']['callback'];
+    }
+
+    /**
+     * Writes a response given the original request and a parameter data accepted by JSON.stringify
+     * @param request
+     * @param response
+     * @param data
+     */
+    function writeJSONPCallback(request, response, data) {
+        if (retrieveCallbackFromRequest(request)) {
+            response.writeHead(200, { 'Content-Type':'application/javascript' });
+            response.end(retrieveCallbackFromRequest(request) + '(' + JSON.stringify(data) + ');');
+        } else {
+            response.writeHead(200, { 'Content-Type':'application/json' });
+            response.end(JSON.stringify(data));
         }
     }
 
     /**
-     * Stub of a functional url cleaner, removes any trailing arguments for routing purposes
-     * @param url
+     * Parses a post string containing JSON and returns a Javascript object
+     * @param post_string
      * @return {*}
+     */
+    function parseRequestPostBody(post_string) {
+        return JSON.parse(require('querystring').parse(post_string).data);
+    }
+
+    /**
+     * Stub of a functional url cleaner, returns the first segment of the URL for basic routing purposes.
+     * @param url
+     * @return string - url path
      */
     function routingPath(url) {
         return url.split(/\?|\//)[1];
     }
 
 
+    /*
+     * Controller actions:
+     * /surveys action
+     */
+
     /**
      * Produces a list of surveys that are available to the client
      * @param response
      */
-    function showSurveyIndex(response) {
+    function showSurveyIndex(request, response) {
         client.query('SELECT id, title FROM surveys', function (error, rows) {
             if (error)
                 throw error;
             else
-                response.end(JSON.stringify(rows));
+                writeJSONPCallback(request, response, rows);
         });
+    }
+
+    /*
+     * /survey/<id> action
+     */
+
+    /**
+     * Retrieves the survey id from the url and calls the function retrieving and presenting the survey.
+     * @param request
+     * @param response
+     */
+    function showSurvey(request, response) {
+        showSingleSurvey(request, request.url.split('/')[2], response);
     }
 
     /**
@@ -53,7 +116,7 @@ exports.start = function () {
      * @param id
      * @param response
      */
-    function showSingleSurvey(id, response) {
+    function showSingleSurvey(request, id, response) {
         client.query("\
             SELECT surveys.id as id, surveys.title as title, \
                 groups.id as group_id, \
@@ -66,13 +129,15 @@ exports.start = function () {
             WHERE surveys.id = ?", [id],
             function (error, rows) {
                 if (error)
-                    response.end(error.toString());
+                    writeJSONPCallback(request, response, {errors : error});
+                else if(!rows.length)
+                    writeJSONPCallback(request, response, {errors : ["Survey not found"]});
                 else
-                    response.end(JSON.stringify({
+                    writeJSONPCallback(request, response, {
                         "id":rows[0]['id'],
                         "title":rows[0]['title'],
                         "groups":createGroupArray(rows)
-                    }));
+                    });
             });
     }
 
@@ -121,6 +186,10 @@ exports.start = function () {
             });
     }
 
+    /*
+     * /completed_survey
+     */
+
     /**
      * Processes a POST request of answers for a given survey.
      * @param request
@@ -132,7 +201,6 @@ exports.start = function () {
             post_data += input.toString();
         });
         request.on('end', function () {
-            console.log("Received post data:", parseRequestPostBody(post_data));
             var output = storeAnswers(parseRequestPostBody(post_data));
             if (output.errors) {
                 console.log("Post had " + output.errors.length + " errors");
@@ -140,17 +208,8 @@ exports.start = function () {
             } else {
                 console.log("Successful post.");
             }
-            response.end(JSON.stringify(output));
+            writeJSONPCallback(request, response, output);
         });
-    }
-
-    /**
-     * Parses a post string containing JSON and returns a Javascript object
-     * @param post_string
-     * @return {*}
-     */
-    function parseRequestPostBody(post_string) {
-        return JSON.parse(require('querystring').parse(post_string).data);
     }
 
     /**
@@ -205,11 +264,15 @@ exports.start = function () {
         });
     }
 
+    /*
+     * /test action
+     */
+
     /**
      * Displays a test page for server requests.
      * @param response
      */
-    function showTestPage(response) {
+    function showTestPage(_, response) {
         require('fs').readFile('./test.html', function (error, content) {
             if (error) {
                 response.writeHead(500);
